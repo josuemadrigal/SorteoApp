2e123204c3ee046866a76e0b64e61c57af1cda29
josuem <josuemadrigal12@gmail.com>
2023-05-16 23:05:12 -0400

    modified:   src/pages/Registros/RegistroPage.tsx

--- src/pages/Registros/RegistroPage.tsx
+++ src/pages/Registros/RegistroPage.tsx
@@ -1,6 +1,7 @@
 
 import { useForm } from "react-hook-form";
-import Swal from 'sweetalert2'
+//import Swal from 'sweetalert2'
+import Swal from 'sweetalert2/dist/sweetalert2.all.js'
 import Box from '@mui/material/Box';
 import { Button, Card, CardContent, CardHeader, Grid, InputLabel, MenuItem, Select, Snackbar, TextField, Alert, AlertTitle } from "@mui/material";
 import RegistrosService from "../../services/RegistrosService";
