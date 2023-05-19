8ee9b10a250895cbfdb399f730af4069320d0850
josuem <josuemadrigal12@gmail.com>
2023-05-18 23:58:19 -0400

    modified:   src/http-common.ts

--- src/http-common.ts
+++ src/http-common.ts
@@ -4,8 +4,9 @@ import { getEnvVariables } from "./helpers/getEnvVariables";
 export const http = axios.create({
     baseURL: getEnvVariables().VITE_API_URL,
     headers: {
-        // 'Access-Control-Allow-Origin': 'http://lxtdnetcor0001:10160',
-        "Content-type": "application/json"
+         'Access-Control-Allow-Origin': '*',
+         'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
+        'Content-type': 'application/json'
 
     },
     withCredentials: false,
