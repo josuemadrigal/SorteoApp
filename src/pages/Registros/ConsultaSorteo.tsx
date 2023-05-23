e01523f00292b021fe1dbd9d04b175dc366be2c7
josuem <josuemadrigal12@gmail.com>
2023-05-23 12:01:15 -0400

    modified:   src/pages/Registros/ConsultaSorteo.tsx

--- src/pages/Registros/ConsultaSorteo.tsx
+++ src/pages/Registros/ConsultaSorteo.tsx
@@ -155,25 +155,7 @@ const checkedItems = checked.length
     height: 300,
   };
 
-  const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        delayChildren: 0.3,
-        staggerChildren: 0.2
-      }
-    }
-  }
-    
-  const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-      y: 0,
-      opacity: 1
-    }
-  }
+
 
   return (
     <Grid container my={4} rowSpacing={2} columnSpacing={1}>
