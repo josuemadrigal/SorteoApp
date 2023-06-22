48cb16358b5fbf7c9412a562d295ca820dfe4508
josuem <josuemadrigal12@gmail.com>
2023-06-22 08:01:42 -0400

    modified:   src/services/RegistrosService.ts

--- src/services/RegistrosService.ts
+++ src/services/RegistrosService.ts
@@ -3,21 +3,21 @@ import { http } from "../http-common"
 
 class RegistrosService{
 
-    public async getRegistros(status:number, municipio:string, cantidad:number, ){
-        const jsonPar:any = {status:status,municipio:municipio, cantidad};
+    public async getRegistros(status: number, municipio: string, cantidad: number, ){
+        const jsonPar:any = {status: status, municipio: municipio, cantidad};
         const params = new URLSearchParams(jsonPar)
         const response = await http.get<any[]>(`/registros?${params}`);
         return response;
     }
 
-    public async crearRegistros(param:any){
+    public async crearRegistros(param: any){
         const response = await http.post<any>("/registros",param);
 
         console.log( response);
         return response;
     }
 
-     async startUpdate (boleta:any, status:number, premio:string) {
+     async startUpdate (boleta: any, status: number, premio: string) {
         return await http.put(`registros/`+boleta, {status , premio});
     }
 
