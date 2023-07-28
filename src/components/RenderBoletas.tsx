0f390cf4c78ec183a68f57ee39e8e8645e2d1a6b
josuem <josuemadrigal12@gmail.com>
2023-07-28 16:53:34 -0400

    renamed:    public/tombolaEduarde.gif -> public/gif-padress.gif 	new file:   public/premios/motor.png 	modified:   src/components/RenderBoletas.tsx 	modified:   src/pages/Registros/ConsultaSorteo.tsx

--- src/components/RenderBoletas.tsx
+++ src/components/RenderBoletas.tsx
@@ -2,33 +2,32 @@ import { useEffect, useState } from "react";
 import { Boleta } from "./Boleta";
 
 interface Props {
-    items: any | any[]
+  items: any | any[];
 }
 export const RenderBoletas = ({ items }: Props) => {
-    const [currentIndex, setCurrentIndex] = useState<number>(0);
-    const [renderedItems, setRenderedItems] = useState<object[]>([]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [renderedItems, setRenderedItems] = useState<object[]>([]);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (currentIndex < items.length) {
-                const currentItem = items[currentIndex];
-                setRenderedItems((prevItems) => [...prevItems, currentItem]);
-                setCurrentIndex((prevIndex) => prevIndex + 1);
-            }
-        }, 2000); // Wait for 30 seconds between each item
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentIndex < items.length) {
+        const currentItem = items[currentIndex];
+        setRenderedItems((prevItems) => [...prevItems, currentItem]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }
+    }, 2000); // Wait for 2 seconds between each item
 
-        return () => {
-            clearInterval(interval);
-        };
-    }, [currentIndex, items]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentIndex, items]);
 
-    return (
-        <>
-            {/* {renderedItems.map((item: object, index: number) => ( */}
-            {renderedItems.map((item: object) => (
-                <Boleta item={item} />
-            ))}
-
-        </>
-    );
-}
+  return (
+    <>
+      {/* {renderedItems.map((item: object, index: number) => ( */}
+      {renderedItems.map((item: object) => (
+        <Boleta item={item} />
+      ))}
+    </>
+  );
+};
--- src/pages/Registros/ConsultaSorteo.tsx
+++ src/pages/Registros/ConsultaSorteo.tsx
@@ -1,186 +1,202 @@
-import {   useState } from "react";
-import '../../App.css';
+import { useState } from "react";
+import "../../App.css";
 
-import GifTombola from "../../../public/tombolaEduarde.gif";
-
+import GifTombola from "../../../public/gif-padress.gif";
 
 // import Lottie, {LottieRefCurrentProps} from 'lottie-react';
 // import animationData from '../../assets/65849-emos-spin.json'
 
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 import { useMutation } from "react-query";
 //import SearchIcon from '@mui/icons-material/Search';
 //import SaveIcon from '@mui/icons-material/Save';
 
-import 'animate.css';
+import "animate.css";
 
-import {  Button, Grid, InputLabel, MenuItem, Paper, Select, TextField, Typography, styled } from "@mui/material";
-import { SelectChangeEvent } from '@mui/material/Select';
-
+import {
+  Button,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
+import { SelectChangeEvent } from "@mui/material/Select";
 
 import registrosService from "../../services/RegistrosService";
 import { useForm } from "react-hook-form";
-
 
 import RegistrosService from "../../services/RegistrosService";
 import Swal from "sweetalert2";
 
 import { RenderBoletas } from "../../components/RenderBoletas";
 
-const modelo = {defaultValues:{
-  municipio:"",
-  premio:"",
-  status :1,
-  cantidad:4
-}}
-
+const modelo = {
+  defaultValues: {
+    municipio: "-",
+    premio: "",
+    status: 1,
+    cantidad: 4,
+  },
+};
 
 export const Consulta = () => {
-
-  const { getValues, register} = useForm(modelo);
+  const { getValues, register } = useForm(modelo);
 
   // const spinRef = useRef<LottieRefCurrentProps>(null);
-  
+
   // const [checked, setChecked] = useState<any[]>([]);
   const [checkList, setCheckList] = useState<any[]>([]);
-  
 
   const [unCheckList, setUnCheckList] = useState<any[]>([]);
-  
-  const [premio, setPremio] = useState('');
 
+  const [premio, setPremio] = useState("");
 
   const handlePremio = (event: SelectChangeEvent) => {
     setPremio(event.target.value);
     console.log(premio);
   };
 
- 
-  
-  const {
-    mutate: getRegistros,
-    
-  } = useMutation<any>( async (param:any)=> await registrosService.getRegistros(param.status,param.municipio, param.cantidad));
+  const { mutate: getRegistros } = useMutation<any>(
+    async (param: any) =>
+      await registrosService.getRegistros(
+        param.status,
+        param.municipio,
+        param.cantidad
+      )
+  );
 
-const CustomGetRegistros = async()=>{
+  const CustomGetRegistros = async () => {
+    setCheckList([]);
 
-  setCheckList([]);
-  
-  setUnCheckList([]);
+    setUnCheckList([]);
 
-  
-  const param:any = getValues();
+    const param: any = getValues();
 
-  if (param.cantidad <=0 ) {
-    return  Swal.fire({
-      position: 'center',
-      icon: 'error',
-      title: 'La cantidad no puede ser 0',
-      showConfirmButton: false,
-      timer: 7000
-    })
-  }
+    if (param.cantidad <= 0) {
+      return Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "La cantidad no puede ser 0",
+        showConfirmButton: false,
+        timer: 7000,
+      });
+    }
 
-  if (param.cantidad > 0 && param.municipio != "") {
+    if (param.cantidad > 0 && param.municipio != "") {
+      const dataa: any = await registrosService.getRegistros(
+        param.status,
+        param.municipio,
+        param.cantidad
+      );
+      await getRegistros(param);
 
-  const dataa:any = await registrosService.getRegistros(param.status,param.municipio, param.cantidad);
-  await getRegistros(param);
+      const a = dataa?.data?.registros?.map((m) => {
+        return m;
+      });
+      // const b = data?.data?.registros.map((datos) => { return datos.cedula});
 
-  
-    const a = dataa?.data?.registros?.map(m => { return m });
-  // const b = data?.data?.registros.map((datos) => { return datos.cedula});
-  
+      setCheckList(a);
+      // setChecked(a);
+    } else {
+      return Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Seleccione el municipio",
+        showConfirmButton: false,
+        timer: 7000,
+      });
+    }
+  };
 
-
-  
-  
-  setCheckList(a);
-  // setChecked(a);
-    
-  } else {
-    return  Swal.fire({
-      position: 'center',
-      icon: 'error',
-      title: 'Seleccione el municipio',
-      showConfirmButton: false,
-      timer: 7000
-    })
-  }
-}
-
-const ActualizarRegistros = ()=>{
-  
-  //Seleccionado
-  for (let index = 0; index < checkList.length; index++) {
-    const element = checkList[index];
-    RegistrosService.startUpdate(String(element.boleta),2, premio);
-  }
-  // No seleccionado
-  for (let index = 0; index < unCheckList.length; index++) {
-    const element = unCheckList[index];
-    RegistrosService.startUpdate(element,0,"No presente");
-  }
-  setCheckList([]);
-  // setChecked([]);
-  setUnCheckList([]);
-}
+  const ActualizarRegistros = () => {
+    //Seleccionado
+    for (let index = 0; index < checkList.length; index++) {
+      const element = checkList[index];
+      RegistrosService.startUpdate(String(element.boleta), 2, premio);
+    }
+    // No seleccionado
+    for (let index = 0; index < unCheckList.length; index++) {
+      const element = unCheckList[index];
+      RegistrosService.startUpdate(element, 0, "No presente");
+    }
+    setCheckList([]);
+    // setChecked([]);
+    setUnCheckList([]);
+  };
 
   const Item = styled(Paper)(({ theme }) => ({
-  
     padding: theme.spacing(1),
-    textAlign: 'center',
-
+    textAlign: "center",
   }));
 
-  
   return (
     <Grid container my={4} rowSpacing={2} columnSpacing={1}>
-      <Grid item md={2} sm={10} >
-      <Item sx={{minHeight:'100%'}}>
-          
-        <Typography gutterBottom variant="h5" component="div">
-          Buscar 
-        </Typography>
-        <InputLabel>Municipio / Distrito</InputLabel>
-        <Select {...register("municipio", {required: true, maxLength: 10}) } color='success'  
-        sx={{minWidth:"40%", width:"100%", margin:"5px 5px 15px 0px" }} >
+      <Grid item md={2} sm={10}>
+        <Item sx={{ height: "550px", position: "fixed", width: "220px" }}>
+          <Typography gutterBottom variant="h5" component="div">
+            Buscar
+          </Typography>
+          <InputLabel>Municipio / Distrito</InputLabel>
+          <Select
+            {...register("municipio", { required: true, maxLength: 10 })}
+            color="success"
+            sx={{ minWidth: "40%", width: "100%", margin: "5px 5px 15px 0px" }}
+          >
+            <MenuItem value="caleta">Caleta</MenuItem>
+            <MenuItem value="cumayasa">Cumayasa</MenuItem>
+            <MenuItem value="guaymate">Guaymate</MenuItem>
             <MenuItem value="la-romana">La Romana</MenuItem>
             <MenuItem value="villa-hermosa">Villa Hermosa</MenuItem>
-            <MenuItem value="caleta">Caleta</MenuItem>
-            <MenuItem value="guaymate">Guaymate</MenuItem>
-            <MenuItem value="cumayasa">Cumayasa</MenuItem>
           </Select>
-          <TextField type="text" placeholder="Cantidad" label="cantidad" color='success'  {...register("cantidad", {required: true, maxLength: 10}) } sx={{minWidth:"20%", width:"100%", margin:"5px 5px 15px 0px" }}/>
-          
-          <Select value={premio} color='success'   onChange={handlePremio} 
-                  sx={{minWidth:"40%", width:"100%", margin:"5px 5px 15px 0px" }} 
+          <TextField
+            type="text"
+            placeholder="Cantidad"
+            label="cantidad"
+            color="success"
+            {...register("cantidad", { required: true, maxLength: 10 })}
+            sx={{ minWidth: "20%", width: "100%", margin: "5px 5px 15px 0px" }}
+          />
+
+          <Select
+            value={premio}
+            color="success"
+            onChange={handlePremio}
+            sx={{ minWidth: "40%", width: "100%", margin: "5px 5px 15px 0px" }}
           >
-            <MenuItem value="setpPaire">Set de Paire</MenuItem>
-            <MenuItem value="setCalderos">Set de Carderos</MenuItem>
-            <MenuItem value="ollasPresion">Ollas de Presion</MenuItem>
-            <MenuItem value="freidora">Freidora</MenuItem>
-            <MenuItem value="vajilla">Vajilla</MenuItem>
-            <MenuItem value="corcha">Corcha</MenuItem>
-            <MenuItem value="cama">Cama</MenuItem>
+            <MenuItem value="motor">Motor</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
+            <MenuItem value="premio">premio</MenuItem>
           </Select>
-          
-          <Button onClick={()=>CustomGetRegistros()} 
-                  variant="contained" 
-                  color='success' 
-                  size="large" 
-                   
-                  sx={{width:"100%"}}>
-                    Buscar</Button>
 
-          <motion.div animate={{ opacity: 1, scale: 1, x: [-150, 0] }}
-                          transition={{ duration: 2 }}>
+          <Button
+            onClick={() => CustomGetRegistros()}
+            variant="contained"
+            color="success"
+            size="large"
+            sx={{ width: "100%", marginBottom: "20px" }}
+          >
+            Buscar
+          </Button>
 
+          <motion.div
+            animate={{ opacity: 1, scale: 1, x: [-150, 0] }}
+            transition={{ duration: 2 }}
+          >
             <img src={`/premios/${premio}.png`} width={"80%"}></img>
+          </motion.div>
 
-
-            </motion.div>
-
-              {/* <div className="checkList">
+          {/* <div className="checkList">
                 <div className="title">Listado De Boletas:</div>
                   <div className="list-container">
                     {checkList && checkList.map((item, index) => (
@@ -193,29 +209,37 @@ const ActualizarRegistros = ()=>{
                 </div>
               </div> */}
 
-              <div>
+          <div>
             {/* <p>{`Boletas presentes:  ${checkedItems}`}</p>  */}
-                {/* <p>{`Boletas ausentes:  ${JSON.stringify( unCheckList)}`}</p> */}
-                
-                <Button onClick={()=>ActualizarRegistros()} 
-                        variant="contained" 
-                        color='error'
-                        size="large"
-                        
-                        sx={{width:"100%"}}>
-                          Guardar
-                  </Button>
-              </div>
-       </Item>
-      </Grid>
-       {/* END CONTROLES */}
+            {/* <p>{`Boletas ausentes:  ${JSON.stringify( unCheckList)}`}</p> */}
 
-       {/* START  Column 2 */}
+            <Button
+              onClick={() => ActualizarRegistros()}
+              variant="contained"
+              color="error"
+              size="large"
+              sx={{ width: "100%" }}
+            >
+              Guardar
+            </Button>
+          </div>
+        </Item>
+      </Grid>
+      {/* END CONTROLES */}
+
+      {/* START  Column 2 */}
       <Grid item md={3}>
-        <Item sx={{minHeight:'100%'}}>
-        <Grid container rowSpacing={5} columnSpacing={1} >
-                      
-          <Grid item md={12}>
+        <Item
+          sx={{
+            height: "550px",
+            position: "fixed",
+            alignItems: "center",
+            width: "260px",
+            marginLeft: "30px",
+          }}
+        >
+          <Grid container rowSpacing={5} columnSpacing={1}>
+            <Grid item md={12}>
               {/* <Lottie onComplete={() => {
                 spinRef.current?.goToAndPlay(45, true)
               }} lottieRef={spinRef} loop={false} style={style} animationData={animationData}/> */}
@@ -231,39 +255,32 @@ const ActualizarRegistros = ()=>{
                 height="360"
               /> */}
 
-              <img src={GifTombola} alt="TOMBOLA" width="90%"  />
+              <img src={GifTombola} alt="TOMBOLA" width="90%" />
+            </Grid>
 
-
-          </Grid>
-
-          {/* <Grid item md={12}>
+            {/* <Grid item md={12}>
                 <Box component="img" src="/foto-eduard2.jpg" alt="hola" sx={{ height: "auto", width: "90%", borderRadius:"10px" }} />
           </Grid> */}
-
-        </Grid>
+          </Grid>
         </Item>
       </Grid>
 
       {/* END  Column 2 */}
 
-
-
-      <Grid item md={7} >
-      <Item sx={{minHeight:'100%'}}>
-      <Typography gutterBottom variant="h5" component="div">
-            GANADORES 
+      <Grid item md={7}>
+        <Item sx={{ minHeight: "500px" }}>
+          <Typography gutterBottom variant="h5" component="div">
+            GANADORES
           </Typography>
 
-        <Grid container rowSpacing={1} columnSpacing={4} >
-
+          <Grid container rowSpacing={1} columnSpacing={4}>
             {checkList.length <= 0 ? (
               <p>.</p>
             ) : (
               <RenderBoletas items={checkList} />
             )}
-          
-        </Grid>
-      </Item>
+          </Grid>
+        </Item>
       </Grid>
     </Grid>
   );
