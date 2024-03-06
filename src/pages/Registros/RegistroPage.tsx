af5c5d36726a865ce8809625d274867a1d60ea17
josuemadrigal <josuemadrigal12@gmail.com>
2024-03-05 20:21:41 -0400

    Mejora de funcionamiento
    Mejor la rapidez de registro y evita errores

--- src/pages/Registros/RegistroPage.tsx
+++ src/pages/Registros/RegistroPage.tsx
@@ -8,18 +8,13 @@ import {
   Card,
   CardContent,
   Grid,
-  InputLabel,
-  MenuItem,
   TextField,
   TextFieldProps,
 } from "@mui/material";
 
-import Select from "@mui/material/Select";
 import RegistrosService from "../../services/RegistrosService";
 import { useRef } from "react";
 import { useLocation, useParams } from "react-router-dom";
-
-// import BarcodeScanner from "../../components/BarcodeScanner";
 
 const modelo = {
   defaultValues: {
@@ -27,200 +22,122 @@ const modelo = {
     municipio: "",
     status: 1,
     premio: "",
+    prefijo: "",
   },
 };
 
 export const Registro = (props: any) => {
-  //const { formState, setValue, getValues, register} = useForm(modelo);
-  const { getValues, register, setValue } = useForm(modelo);
-  const [boleta, setBoleta] = React.useState<string>("");
+  const { getValues, register, handleSubmit, reset } = useForm(modelo);
   const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const location = useLocation();
+
   const { id } = useParams<{ id: string }>();
   const municipioNombre = id || "";
-  const location = useLocation();
+
   const objeto = location.state?.parametros;
 
-  const municipio = objeto.municipio || "";
+  let municipio = objeto?.municipio || "";
 
-  const handleKeyPress = (event) => {
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
-      // Lógica que deseas ejecutar al presionar Enter
-      registerSubmit(event);
+      event.preventDefault(); // Evitar envío automático del formulario
+      handleSubmit(registerSubmit)();
     }
   };
 
-  const handleClic = (event) => {
-    event.key = "Enter";
-    registerSubmit(event);
-  };
+  const registerSubmit = async (objeto: any) => {
+    objeto.municipio = municipioNombre;
 
-  const registerSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+    // Mensajes de error centralizados
+    const errorMessages = {
+      invalidMunicipio: "Seleccione un municipio o distrito válido",
+      invalidBoleta: "Ingrese una boleta válida",
+      duplicateBoleta: "Esta boleta ya ha sido registrada",
+    };
+
+    // Función auxiliar para mostrar errores con Swal
+    const showError = (title: string) => {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title,
+        showConfirmButton: false,
+        timer: 3000,
+      });
+      reset({ boleta: "" });
+    };
 
     try {
-      const objeto = getValues();
+      if (!objeto.municipio || objeto.municipio.length < 1) {
+        showError(errorMessages.invalidMunicipio);
 
-      objeto.municipio = municipioNombre;
-
-      const esLaRomana = objeto.municipio === "la-romana";
-      const esCaleta = objeto.municipio === "caleta";
-      const esVillaHermosa = objeto.municipio === "villa-hermosa";
-      const esCumayasa = objeto.municipio === "cumayasa";
-      const esGuaymate = objeto.municipio === "guaymate";
-
-      const LR = objeto.boleta.slice(0, 2).toUpperCase() === "LR";
-      const CA = objeto.boleta.slice(0, 2).toUpperCase() === "CA";
-      const VH = objeto.boleta.slice(0, 2).toUpperCase() === "VH";
-      const CU = objeto.boleta.slice(0, 2).toUpperCase() === "CU";
-      const GU = objeto.boleta.slice(0, 2).toUpperCase() === "GU";
-
-      if (esLaRomana && !LR) {
-        return Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Boleta no pertenece a La Romana",
-          showConfirmButton: false,
-          timer: 2000,
-        });
+        return;
       }
 
-      if (esCaleta && !CA) {
-        return Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Boleta no pertenece a Caleta",
-          showConfirmButton: false,
-          timer: 2000,
-        });
+      if (objeto.boleta.length !== 8) {
+        showError(errorMessages.invalidBoleta);
+
+        return;
       }
 
-      if (esVillaHermosa && !VH) {
-        return Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Boleta no pertenece a Villa Hermosa",
-          showConfirmButton: false,
-          timer: 2000,
-        });
+      const municipioValido =
+        objeto.municipio === "la-romana" ||
+        objeto.municipio === "caleta" ||
+        objeto.municipio === "villa-hermosa" ||
+        objeto.municipio === "cumayasa" ||
+        objeto.municipio === "guaymate";
+
+      const codigoBoleta = objeto.boleta.slice(0, 2).toUpperCase();
+
+      // Función auxiliar para validar municipio
+      const validarMunicipio = (municipio: string, codigo: string) => {
+        if (objeto.municipio === municipio && codigoBoleta !== codigo) {
+          showError(`Boleta no pertenece a ${municipio.split("-").join(" ")}`);
+          return false;
+        }
+        return true;
+      };
+
+      if (!municipioValido) {
+        showError(errorMessages.invalidMunicipio);
+        return;
       }
 
-      if (esCumayasa && !CU) {
-        return Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Boleta no pertenece a Cumayasa",
-          showConfirmButton: false,
-          timer: 2000,
-        });
-      }
+      if (!validarMunicipio("la-romana", "LR")) return;
+      if (!validarMunicipio("caleta", "CA")) return;
+      if (!validarMunicipio("villa-hermosa", "VH")) return;
+      if (!validarMunicipio("cumayasa", "CU")) return;
+      if (!validarMunicipio("guaymate", "GU")) return;
 
-      if (esGuaymate && !GU) {
-        return Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Boleta no pertenece a Guaymate",
-          showConfirmButton: false,
-          timer: 2000,
-        });
-      }
-
-      if (!objeto.municipio) {
-        return Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Seleccione el municipio o distrito",
-          showConfirmButton: false,
-          timer: 3000,
-        });
-      }
-
-      if (objeto.municipio.length < 1) {
-        return Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Seleccione el municipio o distrito",
-          showConfirmButton: false,
-          timer: 3000,
-        });
-      }
-
-      if (objeto.boleta.length < 8 || objeto.boleta.length > 8) {
-        return Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Ingrese una boleta valida",
-          showConfirmButton: false,
-          timer: 3000,
-        });
-      }
       objeto.status = 1;
       objeto.premio = "";
 
-      Swal.fire({
-        title: "¿Están correctos sus datos?",
-        html: `<h4> Municipio: <b>${municipio}</b></h4>
-              <h4> # Boleta: <b>${objeto.boleta}</b></h4>`,
+      const response = await RegistrosService.crearRegistros(objeto);
 
-        icon: "question",
-        showCancelButton: true,
-        cancelButtonText: "Editar datos",
-        confirmButtonColor: "#2e7d32",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Confirmar",
-      }).then(async (result) => {
-        if (result.isConfirmed) {
-          const response = await RegistrosService.crearRegistros(objeto);
+      if (response.status === 203) {
+        showError(errorMessages.duplicateBoleta);
+        reset({ boleta: "" });
+      }
 
-          if (response.status == 203) {
-            Swal.fire({
-              position: "center",
-              icon: "error",
-              title: "Intente más tarde",
-              showConfirmButton: false,
-              timer: 7000,
-            });
-          }
+      if (response.status === 201) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: `Registro de la boleta ${objeto.boleta} COMPLETADO`,
+          showConfirmButton: false,
+          timer: 4000,
+        });
 
-          if (response.status == 201) {
-            Swal.fire({
-              position: "center",
-              icon: "success",
-              title: `Registro de la boleta ${objeto.boleta} COMPLETADO`,
-              showConfirmButton: false,
-              timer: 3000,
-            });
-
-            if (inputRef.current) {
-              inputRef.current.focus();
-              setValue("boleta", "");
-            }
-            // return setTimeout(() => {
-            //   window.location.replace(
-            //     "https://www.instagram.com/eduardespiritusanto/"
-            //   );
-            // }, 2000);
-          }
-
-          if (response.status == 203) {
-            Swal.fire({
-              position: "center",
-              icon: "error",
-              title: "Esta boleta ya ha sido registrada",
-              showConfirmButton: false,
-              timer: 7000,
-            });
-          }
+        if (inputRef.current) {
+          inputRef.current.focus();
         }
-      });
+        // Limpiar el campo de la boleta
+        reset({ boleta: "" });
+      }
     } catch (error) {
       console.log(error);
-      return Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "Intente más tarde",
-        showConfirmButton: false,
-        timer: 9000,
-      });
+      showError("Intente más tarde");
     }
   };
 
@@ -232,11 +149,6 @@ export const Registro = (props: any) => {
         alignItems="center"
         style={{
           minHeight: "100vh",
-          // minWidth: "",
-          // backgroundImage: `url(/fondoE.jpg)`,
-          // backgroundSize: "cover",
-          // backgroundPosition: "center",
-
           margin: 0,
         }}
       >
@@ -262,57 +174,43 @@ export const Registro = (props: any) => {
 
             <CardContent>
               <h1>{municipio}</h1>
-              {/* <InputLabel id="demo-multiple-name-label">
-                Municipio / Distrito
-              </InputLabel>
-              <Select
-                variant="filled"
-                {...register("municipio", { required: true })}
-                color="success"
-                sx={{ minWidth: "100%", margin: "5px 5px 15px 0px" }}
-              >
-                <MenuItem value="caleta">Caleta</MenuItem>
-                <MenuItem value="cumayasa">Cumayasa</MenuItem>
-                <MenuItem value="guaymate">Guaymate</MenuItem>
-                <MenuItem value="la-romana">La Romana</MenuItem>
-                <MenuItem value="villa-hermosa">Villa Hermosa</MenuItem>
-              </Select> */}
 
-              <InputMask
-                // alwaysShowMask
-                {...props}
-                mask="aa999999"
-                maskChar=""
-                maskPlaceholder={null}
-                value={boleta}
-                required
-                {...register("boleta", { required: true, maxLength: 11 })}
-                sx={{ minWidth: "100%", margin: "5px 5px 15px 0px" }}
-                onChange={(e) => setBoleta(e.target.value)}
-              >
-                {(inputProps: Props & TextFieldProps) => (
-                  <TextField
-                    {...inputProps}
-                    variant="filled"
-                    color="success"
-                    type="text"
-                    label="Número de boleta"
-                    inputRef={register("boleta")}
-                    ref={inputRef}
-                    onKeyDown={handleKeyPress}
-                  />
-                )}
-              </InputMask>
-              {/* <BarcodeScanner /> */}
-              <Button
-                variant="contained"
-                color="success"
-                type="submit"
-                onClick={handleClic}
-                sx={{ minWidth: "100%", margin: "5px 5px 15px 0px" }}
-              >
-                Registrar
-              </Button>
+              <form onSubmit={(e) => e.preventDefault()}>
+                <InputMask
+                  {...props}
+                  mask="aa999999"
+                  maskChar=""
+                  maskPlaceholder={null}
+                  required
+                  {...register("boleta", { required: true, maxLength: 11 })}
+                  sx={{ minWidth: "100%", margin: "5px 5px 15px 0px" }}
+                  inputRef={(node) => {
+                    register(node);
+                    inputRef.current = node;
+                  }}
+                  onKeyDown={handleKeyPress}
+                >
+                  {(inputProps: Props & TextFieldProps) => (
+                    <TextField
+                      {...inputProps}
+                      variant="filled"
+                      color="success"
+                      type="text"
+                      label="Número de boleta"
+                    />
+                  )}
+                </InputMask>
+
+                <Button
+                  variant="contained"
+                  color="success"
+                  type="submit"
+                  sx={{ minWidth: "100%", margin: "5px 5px 15px 0px" }}
+                  onClick={handleSubmit(registerSubmit)}
+                >
+                  Registrar
+                </Button>
+              </form>
             </CardContent>
           </Card>
         </Grid>
@@ -320,5 +218,3 @@ export const Registro = (props: any) => {
     </>
   );
 };
-
-//export default Registro;
