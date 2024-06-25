300a4b2c7c3bd03ec7b97e48c53a11fb72c511a7
josuemadrigal <josuemadrigal12@gmail.com>
2024-06-25 10:58:27 -0400

    restartt cors

--- src/http-common.ts
+++ src/http-common.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { getEnvVariables } from "./helpers/getEnvVariables";
 
 export const http = axios.create({
-  // baseURL: getEnvVariables().VITE_API_URL,
   baseURL: getEnvVariables().VITE_API_URL,
   headers: {
-    //'Access-Control-Allow-Origin': '*',
-    //'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
     "Content-type": "application/json",
   },
   withCredentials: false,
