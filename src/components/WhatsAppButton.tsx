c737a1b340f68475a19f5da6cdd973dd7af8a565
josuemadrigal <josuemadrigal12@gmail.com>
2024-07-18 12:14:29 -0400

    Update

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import CloseIcon from "@mui/icons-material/Close";
 import { motion, AnimatePresence } from "framer-motion";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import "./WhatsAppButton.css";
 
-const WhatsAppButton: React.FC = () => {
+const WhatsAppButton = () => {
   const [showMessage, setShowMessage] = useState(false);
-  const phoneNumber = "18298074136";
+  const [anchorEl, setAnchorEl] = useState(null); // Estado para el elemento ancla del menú
+  const phoneNumber = "18092846378";
   const message = "Hola, necesito ayuda para registrarme en el sorteo.";
 
   useEffect(() => {
@@ -17,9 +20,24 @@ const WhatsAppButton: React.FC = () => {
     return () => clearTimeout(timer);
   }, []);
 
-  const handleClose = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    setShowMessage(false);
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget); // Abrir el menú en la posición del clic
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null); // Cerrar el menú al seleccionar una opción
+  };
+
+  const handleContact = (phoneNumber) => {
+    handleCloseMenu(); // Cerrar el menú al seleccionar una opción
+    handleWhatsAppRedirect(phoneNumber); // Redirigir automáticamente al hacer clic en una opción
+  };
+
+  const handleWhatsAppRedirect = (phoneNumber) => {
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      message
+    )}`;
+    window.open(whatsappUrl, "_blank");
   };
 
   return (
@@ -44,27 +62,46 @@ const WhatsAppButton: React.FC = () => {
               <p style={{ fontSize: "0.875rem", marginRight: "1rem" }}>
                 🌟 ¿Necesitas ayuda para registrarte? ¡Escríbenos! 📲
               </p>
-              <button onClick={handleClose} className="close-button">
+              <button
+                onClick={() => setShowMessage(false)}
+                className="close-button"
+              >
                 <CloseIcon style={{ height: "1rem", width: "1rem" }} />
               </button>
             </div>
           </motion.div>
         )}
       </AnimatePresence>
-      <motion.a
-        href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(
-          message
-        )}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="whatsapp-button"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        onMouseOver={() => setShowMessage(false)}
-        onMouseOut={() => setShowMessage(true)}
-      >
-        <WhatsAppIcon style={{ height: "2rem", width: "2rem" }} />
-      </motion.a>
+      <div className="whatsapp-button-container">
+        <motion.button
+          onClick={handleOpenMenu}
+          className="whatsapp-button"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          aria-controls="whatsapp-menu"
+          aria-haspopup="true"
+        >
+          <WhatsAppIcon style={{ height: "2rem", width: "2rem" }} />
+        </motion.button>
+        <Menu
+          id="whatsapp-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleCloseMenu}
+          PaperProps={{
+            style: {
+              marginTop: "0.5rem",
+            },
+          }}
+        >
+          <MenuItem onClick={() => handleContact("18298074136")}>
+            Opción 1
+          </MenuItem>
+          <MenuItem onClick={() => handleContact("18096721235")}>
+            Opción 2
+          </MenuItem>
+        </Menu>
+      </div>
     </>
   );
 };
