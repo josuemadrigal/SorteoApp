70cb6c9f14247d48d7a8d65c74cf1e9f80ef9f90
josuemadrigal <josuemadrigal12@gmail.com>
2025-04-09 22:35:46 -0400

    update server host

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import process from "process";
+
+const port = parseInt(process.env.VITE_PORT || "5173");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port,
     allowedHosts: ["app.eduardespiritusanto.com"],
   },
 });
