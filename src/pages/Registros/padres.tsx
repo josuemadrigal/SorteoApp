9d2913ade5f8be2503919a174398225508b168f3
josuemadrigal <josuemadrigal12@gmail.com>
2025-05-16 10:06:16 -0400

    Update

--- src/pages/Registros/padres.tsx
+++ src/pages/Registros/padres.tsx
@@ -1,54 +1,54 @@
-import { Grid, Typography } from "@mui/material";
-import gifLoading from "../../../public/padres.gif";
+// import { Grid, Typography } from "@mui/material";
+// import gifLoading from "../../../public/padres.gif";
 
-const HomePadres = () => {
-  return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: "100vh" }}
-      spacing={2}
-    >
-      <Grid item>
-        <Typography
-          style={{ fontWeight: "bold", textTransform: "uppercase" }}
-          variant="h3"
-          gutterBottom
-          textAlign="center"
-        >
-          Eduard Web App
-        </Typography>
-      </Grid>
-      <Typography
-        style={{
-          fontWeight: "bold",
-          textTransform: "uppercase",
-          fontSize: "25px",
-          marginTop: "55px",
-          color: "white",
-          backgroundColor: "seagreen",
-          padding: "10px",
-          borderRadius: "10px",
-        }}
-        gutterBottom
-        textAlign="center"
-      >
-        Sorteo Padres en desarrollo
-      </Typography>
-      <Grid
-        style={{
-          display: "block",
-          alignContent: "center",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <img src={gifLoading} alt="loading" style={{ width: "300px" }} />
-      </Grid>
-    </Grid>
-  );
-};
+// const HomePadres = () => {
+//   return (
+//     <Grid
+//       container
+//       direction="column"
+//       alignItems="center"
+//       justifyContent="center"
+//       style={{ minHeight: "100vh" }}
+//       spacing={2}
+//     >
+//       <Grid item>
+//         <Typography
+//           style={{ fontWeight: "bold", textTransform: "uppercase" }}
+//           variant="h3"
+//           gutterBottom
+//           textAlign="center"
+//         >
+//           Eduard Web App
+//         </Typography>
+//       </Grid>
+//       <Typography
+//         style={{
+//           fontWeight: "bold",
+//           textTransform: "uppercase",
+//           fontSize: "25px",
+//           marginTop: "55px",
+//           color: "white",
+//           backgroundColor: "seagreen",
+//           padding: "10px",
+//           borderRadius: "10px",
+//         }}
+//         gutterBottom
+//         textAlign="center"
+//       >
+//         Sorteo Padres en desarrollo
+//       </Typography>
+//       <Grid
+//         style={{
+//           display: "block",
+//           alignContent: "center",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <img src={gifLoading} alt="loading" style={{ width: "300px" }} />
+//       </Grid>
+//     </Grid>
+//   );
+// };
 
-export default HomePadres;
+// export default HomePadres;
